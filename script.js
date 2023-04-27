690518021ce912253f235600d9dc6c564bb1a0a9
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-27 12:34:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
   
-  
+const samp = document.getElementById('sampleTable');
+  const newRow = samp.insertRow(0);
+  const leftCell = newRow.insertCell(0);
+  const rightCell = newRow.insertCell(1);
+  leftCell.innerHTML = 'New Cell1';
+  rightCell.innerHTML = 'New Cell2';
 }
